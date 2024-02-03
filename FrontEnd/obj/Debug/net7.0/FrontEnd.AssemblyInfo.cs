4805dfe5904f7d55f5560e916c70ca1f3d6da4ad
Lawrence <lorenzo.joseph.dominguez@gmail.com>
2024-02-03 09:26:34 -0800

    client update

--- FrontEnd/obj/Debug/net7.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net7.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8961bc420be6ba49c15aa03db1c9adda57960d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
