08cf4e6d6c1a41cb0bf46c1dbeab17bd6e032886
Lawrence <lorenzo.joseph.dominguez@gmail.com>
2024-03-01 18:42:02 -0800

    update branch

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33649e9b3445278ff2291afe4b1fe93e5d699c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
